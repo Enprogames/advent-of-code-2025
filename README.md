b9af9da713d8a96cf6f55b2ac10ef68d91a45633
Ethan Posner <e_posner@outlook.com>
2025-12-01 20:35:44 -0800

    Added initial files

---
+++ README.md
@@ -0,0 +1,15 @@
+# My Advent of Code 2025 Solutions (Just for Fun) using Rust
+
+[![advent-of-code-2025](https://img.shields.io/badge/Advent_of_Code-2025-blue?style=flat)]((https://adventofcode.com))
+
+My private leaderboard: `4319765-535c51ef`
+
+| Day | Code                                    |                  Stars                   |
+| ---:|-----------------------------------------|:----------------------------------------:|
+| 1   | [src/bin/01.rs](src/bin/01.rs) |                   ⭐️⭐️                   |
+| 2   | [src/bin/02.rs](src/bin/02.rs) |                    ⭐️                    |
+| 3   | [src/bin/03.rs](src/bin/03.rs) |                   ⭐️⭐️                   |
+| 4   | [src/bin/04.rs](src/bin/04.rs) |                                          |
+| 5   | [src/bin/05.rs](src/bin/05.rs) |                    ⭐️                    |
+
+Created from [Template for solving Advent of Code puzzles in Rust with RustRover](https://github.com/bravit/advent-of-code-rust-template)
---
+++ src/bin/NN.rs
@@ -0,0 +1,65 @@
+use anyhow::*;
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+use std::time::Instant;
+use code_timing_macros::time_snippet;
+use const_format::concatcp;
+use advent_of_code_2025::*;
+
+const DAY: &str = "NN"; // TODO: Fill the day
+const INPUT_FILE: &str = concatcp!("input/", DAY, ".txt");
+// TODO: Get big boy input
+// const INPUT_FILE_BIG_BOY: &str = concatcp!("input/", "bigboy", DAY, ".txt");
+
+const TEST: &str = r#"
+
+"#; // TODO: Enter test input
+
+fn part1<R: BufRead>(reader: R) -> Result<usize> {
+    // TODO: Solve Part 1 of the puzzle
+    let answer = reader.lines().flatten().count();
+    Ok(answer)
+}
+
+fn part2<R: BufRead>(reader: R) -> Result<usize> {
+    // TODO: Solve Part 2 of the puzzle
+    Ok(0)
+}
+
+fn main() -> Result<()> {
+    start_day(DAY);
+
+    //region Part 1
+    println!("=== Part 1 ===");
+
+    assert_eq!(2, part1(BufReader::new(TEST.as_bytes()))?);
+
+    let input_file = BufReader::new(File::open(INPUT_FILE)?);
+    let result = time_snippet!(part1(input_file)?);
+    println!("Result = {}", result);
+
+    // TODO: Uncomment for big boy result
+    // let result = time_snippet!(part1(
+    //     BufReader::new(File::open(INPUT_FILE_BIG_BOY)?)
+    // )?);
+    // println!("Result (big boy) = {}", result);
+    //endregion
+
+    //region Part 2
+    // println!("\n=== Part 2 ===");
+    //
+    // assert_eq!(0, part2(BufReader::new(TEST.as_bytes()))?);
+    //
+    // let input_file = BufReader::new(File::open(INPUT_FILE)?);
+    // let result = time_snippet!(part2(input_file)?);
+    // println!("Result = {}", result);
+
+    // TODO: Uncomment for big boy result
+    // let result = time_snippet!(part2(
+    //     BufReader::new(File::open(INPUT_FILE_BIG_BOY)?)
+    // )?);
+    // println!("Result (big boy) = {}", result);
+    //endregion
+
+    Ok(())
+}
