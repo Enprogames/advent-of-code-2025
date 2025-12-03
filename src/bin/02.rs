221f825d4b6b88e58c657e674b819baf314ccc09
Ethan Posner <e_posner@outlook.com>
2025-12-02 21:11:03 -0800

    WIP: Day 2 implementation

--- src/bin/02.rs
+++ src/bin/02.rs
@@ -5,18 +5,61 @@ use std::time::Instant;
 use code_timing_macros::time_snippet;
 use const_format::concatcp;
 use advent_of_code_2025::*;
+use rayon::prelude::*;
 
-const DAY: &str = "02"; // TODO: Fill the day
+const DAY: &str = "02";
 const INPUT_FILE: &str = concatcp!("input/", DAY, ".txt");
 // TODO: Get big boy input
 // const INPUT_FILE_BIG_BOY: &str = concatcp!("input/", "bigboy", DAY, ".txt");
 
 const TEST: &str = r#"
-
+11-22,95-115,998-1012,1188511880-1188511890,222220-222224,
+1698522-1698528,446443-446449,38593856-38593862,565653-565659,
+824824821-824824827,2121212118-2121212124
 "#; // TODO: Enter test input
 
 fn part1<R: BufRead>(reader: R) -> Result<usize> {
     // TODO: Solve Part 1 of the puzzle
+
+    fn count_both_sides_equal_occurences(start: &str, end: &str) -> usize {
+        let count = 0;
+        
+        // Skip this if both sides are odd-length and no even-length values exist in-between.
+        // An odd-length number cannot have both sides equal.
+        if start.len() == end.len() && end.len() % 2 == 1 {
+            return 0
+        }
+
+        let (start_val, start_len) = parse_int_ascii(start.as_bytes());
+        let (end_val, end_len) = parse_int_ascii(end.as_bytes());
+
+        for val in start..=end {
+            let current_length_reference = 1;
+            let current_length = 0;
+
+            if 
+        }
+
+        count
+    }
+    
+    reader
+        .lines()
+        .filter_map(|line| line.ok())
+        .collect::<String>()
+        .split(",")
+        .collect::<Vec<&str>>()
+        .par_iter()
+        .map(|range_str|  // Get left and right items in the string
+        {
+            let range_split = range_str.split("-");
+            let left = range_split.next()?;
+            let right = range_split.next()?;
+
+            count_both_sides_equal_occurences(left, right)
+        })
+        .sum();
+
     let answer = reader.lines().flatten().count();
     Ok(answer)
 }
--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,26 @@ pub fn start_day(day: &str) {
 
 // Additional common functions
 
+/// Much faster than parse
+///
+/// # Arguments
+/// * slice: The bytes to parse. MUST only contain the digits of the number
+/// 
+/// # Returns
+/// Pair where first value is the actual number, and second value is the length of the number
+pub fn parse_int_ascii(slice: &[u8]) -> (usize, usize) {
+    let mut val = 0;
+    let mut len = 0;
+
+    while len < slice.len() {
+        val *= 10;  // Shift decimal space
+        val += (slice[len] - b'0') as usize;  // 
+        len += 1;
+    }
+
+    (val, len)
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
